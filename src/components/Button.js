e861a045216d60b9d0d5d040f0ca902a1c3f8752
Warri <warriisaac@outlook.com>
2019-11-24 02:30:30 +0100

    product and detail component is completed

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,15 +5,16 @@ export const ButtonContainer = styled.button `
     font-size: 1.4rem;
     background: transparent;
     border:0.05rem solid var(--lightBlue);
-    color:var(--lightBlue);
+    border-color:${props => props.cart? "var(--mainBlue)":"var(--lightBlue)"}; 
+    color:${props => props.cart? "var(--mainBlue)":"var(--lightBlue)"};
     border-radius:0.5rem;
     padding: 0.2rem 0,5rem;
     margin: 0.2rem 0.5rem 0;
     cursor: pointer;
     transition: all 0.5s ease-in;
     &:hover {
-        background: var(--lightBlue);
-        color:var(--mainBlue);
+        background: ${props => props.cart? "var(--mainBlue)":"var(--lightBlue)"};
+        color:var(--darkGreen);
     }
     &:focus {
         outline:none;
--- src/components/Product.js
+++ src/components/Product.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import { ProductConsumer } from './Context';
+import PropTypes from 'prop-types';
 
 export default class Product extends Component {
   render() {
@@ -9,17 +10,29 @@ export default class Product extends Component {
     return (
       <ProductContainer className="col-9 mx-auto col-md-6 col-lg-3 my-3"> 
         <div className="card">
-          <div className="img-container p-5" onClick={() => console.log('hey, you click the image container')}>
-              
-              <Link to="/details">
-                <img src={img} alt="product" className="card-img-top" />
-              </Link>
-              <button className="card-btn" disabled={inCart ? true : false} onClick={() => console.log('added to cart')}>
-                {inCart?(<p className="text-capitalize mb-0" disabled>
-                  in Cart
-                </p>): <i className="fa fa-cart-plus"></i>}
-              </button>
-          </div>
+          <ProductConsumer>
+            {(value)=> (
+            <div className="img-container p-5" 
+              onClick={() => 
+                value.handleDetail(id)
+              }
+            >
+                
+                <Link to="/details">
+                  <img src={img} alt="product" className="card-img-top" />
+                </Link>
+                <button className="card-btn" disabled={inCart ? true : false} 
+                  onClick={() => 
+                    value.addToCart(id) 
+                  }
+                  >
+                  {inCart?(<p className="text-capitalize mb-0" disabled>
+                    in Cart
+                  </p>): <i className="fa fa-cart-plus"></i>}
+                </button>
+            </div>
+            )}
+          </ProductConsumer>
           {/* {card footer} */}
           <div className="card-footer d-flex justify-content-between">
             <p className="align-self-center mb-0">{title}</p>
@@ -32,6 +45,16 @@ export default class Product extends Component {
       </ProductContainer>
     );
   }
+}
+
+Product.propTypes = {
+  product:PropTypes.shape({
+    id:PropTypes.number,
+    img:PropTypes.string,
+    title:PropTypes.string,
+    price:PropTypes.number,
+    inCart:PropTypes.bool
+  }).isRequired
 }
 
 const ProductContainer = styled.div`
